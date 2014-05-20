c520fb6b356669246ba7f38c91decd687853edac
ivan-g-v <veloso_32@hotmail.com>
2014-05-20 18:37:24 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,30 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+## In this file I have two functions. With these two functions I 
+## try to obtain the inverse matrix of a square matrix that 
+## somebody write
+
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## In this first function it creates a special "matrix", which is really a list containing a function to
+## set the value of the vector
+## get the value of the vector
+## set the value of the mean
+## get the value of the mean
 
+makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmean <- function(mean) m <<- mean
+        getmean <- function() m
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean)
 }
 
 
